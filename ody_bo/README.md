7cfae7bb4e22b69225161d6cc724ddbb6b0a5d35
Benjamin Sanchez-Lengeling <beangoben@gmail.com>
2017-04-10 11:14:11 -0400

    bo ready"

--- ody_bo/README.md
+++ ody_bo/README.md
@@ -8,9 +8,10 @@ Use for optimizing the hyperparameters for a variational autoencoder on MNIST da
 
 To use it, you will want to modify **bo_utils.py**. Three folder locations are important:
 
-1. **remote odyssey folder**, this is where you will run your job submitter.
-2. **local sshfs folder**, where you mount the remote folder to receive and send files.
-3. **local spearmint folder**, where you will run the docker image.
+1. **remote odyssey folder**, this is where you will run your job submitter (*/n/odyssey/jobs* below).
+2. **local sshfs folder**, where you mount the remote folder to receive and send files (*/home/beangoben/projects/test/jobs* below).
+3. **local spearmint folder**, where you will run the docker image
+(*/home/beangoben/projects/spearmint_docker/ody_bo* below).
 
 Following are the steps to run it:
 
@@ -24,7 +25,7 @@ Following are the steps to run it:
 
 * **bo_loop.py** is the bo file that imports **bo_utils.py** to do the whole bo loop. Not much editing is needed here.
 * **bo_utils.py** is a python module used in bo_loop.py, it has functions for :
-  1. (`create_job(job_id, param, job_dir)`) : Creating all the job files neeeded to run in your supercomputer 
+  1. (`create_job(job_id, param, job_dir)`) : Creating all the job files neeeded to run in your supercomputer
   2. (`parse_job(job_id, job_dir)`): Parsing calculations for results from a job, will be looking for a string called " Final Result = ".
   3. (`not_ready(job_id, job_dir)`): Determine when a calculation is ready to be parsed, basically scanning the result file for a string such as "\*\*FINISHED\*\*".
   4. Also contains other utility functions.
@@ -35,21 +36,21 @@ Following are the steps to run it:
 Assuming you are in a directory inside of your favorite supercomputer (maybe odyssey) you run a command to start your automated job submitter, for example:
 
 ```
-python job_manager.py -r "$(pwd)"/Jobs
+python job_manager.py -r "$(pwd)"/jobs
 ```
 
 ## Mount virtual drive locally
 Using sshfs, assuming *path_to_jobs* is the previous folder location in the supercomputer.
 ```
-sshfs user@domain:path_to_jobs/Jobs "$(pwd)"/Jobs
+sshfs user@domain:/n/odyssey/jobs /home/beangoben/projects/test/jobs
 ```
 ## Run docker image on this folder
-The last number is optional, this is the port number for the docker server.
+An extra last number is optional (i.g. 8890), this is the port number for the docker server.
 ```
-./run_spearmint.sh ody_bo 8890
+./run_spearmint.sh ody_bo
 ```
 ## Run job_watcher.py
 
 ```
-python job_watcher.py
+python job_watcher.py /home/beangoben/projects/spearmint_docker/ody_bo /home/beangoben/projects/test/jobs
 ```
--- ody_bo/job_watcher.py
+++ ody_bo/job_watcher.py
@@ -3,61 +3,67 @@ import shutil
 import importlib.util
 import time
 import sys
-
-# local directory where odyssey was mounted
-CALC_DIR = '/home/beangoben/projects/test/jobs'
-# local directory for docker/speamint experiment
-BO_DIR = '/home/beangoben/projects/spearmint_docker/ody_bo'
-bo_job_dir = os.path.join(BO_DIR, 'jobs')
-# load module of utilities
-spec = importlib.util.spec_from_file_location(
-    "bo_utils", "{}/bo_utils.py".format(BO_DIR))
-bo_utils = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(bo_utils)
+import argparse
 
 
-def check_new(job_id):
+def check_new(calc_dir, bo_job_dir, job_id):
     job_pre = 'job_{:d}'.format(job_id)
     new_dir = os.path.join(bo_job_dir, job_pre)
     if os.path.isdir(new_dir):
         time.sleep(5)
-        inbox_dir = os.path.join(CALC_DIR, 'inbox')
+        inbox_dir = os.path.join(calc_dir, 'inbox')
         print('mv job_{:d}'.format(job_id), end='')
         shutil.move(new_dir, inbox_dir)
     return
 
 
-def check_result(job_id):
+def check_result(calc_dir, bo_job_dir, job_id):
     # check if result exists
     dst_file = os.path.join(bo_job_dir, 'results.out')
     if not os.path.exists(dst_file):
         # check if the folder is ready
-        done_dir = os.path.join(CALC_DIR, 'completed',
+        done_dir = os.path.join(calc_dir, 'completed',
                                 'job_{:d}'.format(job_id))
         if bo_utils.is_ready(done_dir):
             src_file = os.path.join(done_dir, 'results.out')
             shutil.copy(src_file, dst_file)
-            copy_file = os.path.join(bo_job_dir, 'results_parsed.out')
+            copy_file = os.path.join(done_dir, 'results_parsed.out')
             shutil.move(src_file, copy_file)
             print('mv result_{:d}'.format(job_id), end='')
     return
 
 
-def main():
+def main(calc_dir, bo_dir):
+    bo_job_dir = os.path.join(bo_dir, 'jobs')
     count = 0
     while True:
         job_id = bo_utils.read_iteration(bo_job_dir)
         # check if there is a new job_calcution ready
-        check_new(job_id)
+        check_new(calc_dir, bo_job_dir, job_id)
         # check if there is a new result
-        check_result(job_id)
+        check_result(calc_dir, bo_job_dir, job_id)
         # time looping
         pend = '\n' if count % 20 == 19 else ''
         print('.', end=pend)
         sys.stdout.flush()
         count += 1
-        time.sleep(5)
+        time.sleep(30)
     return
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description='watch jobs from a job manager')
+    parser.add_argument('spearmint_dir', type=str,
+                        help='a file path containing a spearmint experiment')
+    parser.add_argument('job_dir', type=str,
+                        help='a file path where to transfer and monitor the jobs')
+    global bo_utils
+    args = parser.parse_args()
+    # load module of utilities
+    spec = importlib.util.spec_from_file_location(
+        "bo_utils", "{}/bo_utils.py".format(args.spearmint_dir))
+    bo_utils = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(bo_utils)
+
+    main(calc_dir=args.job_dir,
+         bo_dir=args.spearmint_dir)
